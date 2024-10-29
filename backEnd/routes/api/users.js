0b86d3b215315c5e2abc0f04b17676fcf1f2f1db
mDestaFadilah <desta.08b@gmail.com>
2024-10-30 02:45:13 +0700

    validation input

--- backEnd/routes/api/users.js
+++ backEnd/routes/api/users.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const Users = require("../../models/Users");
-
+const { check, ValidationResult, validationResult } = require("express-validator");
 
 // @route   GET api/users
 // @desc    test user routes
@@ -12,7 +12,20 @@ router.get('/',(req,res) => res.send("User route"));
 // @route   POST api/users
 // @desc    post users routes
 // @access  Public
-router.post('/', async (req,res) => {
+router.post('/', 
+[
+    check('email','Email harus valid').isEmail(),
+    check('description','Deskripsi wajib isi').not().isEmpty(),
+    check('password','Password harus minimal 4 karakter').isLength({
+        min: 4,
+    })
+],
+async (req,res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()})
+    }
+
     const { email, date, description, password } = req.body;
     try {
         let user = await Users.findOne({ email });
