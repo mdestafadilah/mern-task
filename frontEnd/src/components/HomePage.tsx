e88c04249f439b3795a05d16e3040058dc883ee7
mDestaFadilah <desta.08b@gmail.com>
2024-10-30 12:49:43 +0700

    mockup component

---
+++ frontEnd/src/components/HomePage.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const HomePage: FC = () => {
+    return (
+        <>
+            <h1>Welcome to Task Home Page...</h1>
+        </>
+    )
+}
+
+export default HomePage;
---
+++ frontEnd/src/components/Task.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const Task: FC = () => {
+    return (
+        <>
+            <h1>Welcome to Task Home Page...</h1>
+        </>
+    )
+}
+
+export default Task;
